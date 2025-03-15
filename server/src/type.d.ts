23fac5ad843c56d27324d9a6a8335c457fad747a
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-03-16 00:52:57 +0700

    update type server

--- server/src/type.d.ts
+++ server/src/type.d.ts
@@ -3,16 +3,6 @@ import { TokenPayload } from './models/request/User.request'
 
 declare module 'express' {
   interface Request {
-    user?: User
-    decode_authorization?: TokenPayload
-    decoded_refresh_token?: TokenPayload
-    decoded_email_verify_token?: TokenPayload
-    decode_forgot_password_token?: TokenPayload
-    user_role?: number
-  }
-}
-declare module 'express-serve-static-core' {
-  interface Request {
     user?: any
     user_id?: string
     user_role?: number
